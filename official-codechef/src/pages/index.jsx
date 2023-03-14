2652720d7702088464685feeb1dee3c91edb4921
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2023-03-14 11:36:23 +0530

    tailwind error solved

--- official-codechef/src/pages/index.jsx
+++ official-codechef/src/pages/index.jsx
@@ -4,6 +4,8 @@ import { Poppins } from "next/font/google";
 import Depart_Lead from "../components/Home/Depart_Lead"
 import ContactUs from "../components/Home/ContactUs";
 import Events from "@/components/Home/Events";
+import HeroSection from "@/components/Home/HeroSection";
+
 const inter = Poppins({ subsets: ["latin"],
 weight: '400' });
 
@@ -11,6 +13,7 @@ export default function Home() {
   return (
     <main className={inter.className}>
       {/* <h1 className={inter.className}>Home Page</h1> */}
+      <HeroSection/>
       <Depart_Lead/>
       <Events/>
       <ContactUs/>
