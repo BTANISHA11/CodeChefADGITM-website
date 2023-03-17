85ae293e4920fe5d391e8e15f22d632c3ecfc2f5
A1Kumari <aryakumari3953@gmail.com>
2023-03-17 20:13:14 +0530

    team member

--- official-codechef/src/components/Home/Depart_Lead.jsx
+++ official-codechef/src/components/Home/Depart_Lead.jsx
@@ -3,41 +3,10 @@ import styles from "../../styles/Depart.module.css";
 import Image from 'next/image';
 import MngtImg from "../../../public/assets/Mngt.png";
 import Team_img from "../../../public/assets/team.png";
-import Members from "../../members/TeamMembers.jsx";
-
-
-    const teamMembers = [
-      {
-        name: "John Doe",
-        stack: "Frontend Developer",
-        info: "lorem ipsum",
-        img: "Team_member_img"
-      },
-      {
-        name: "Arya",
-        stack: "Backend Developer",
-        info: "lorem ipsum",
-        img: "https://example.com/jane-doe.jpg"
-      },
-      {
-        name: "Arya",
-        stack: "Backend Developer",
-        info: "lorem ipsum",
-        img: "https://example.com/jane-doe.jpg"
-      },
-      {
-        name: "Arya",
-        stack: "Designer",
-        info: "For the CSS, you can modify it as per your design requirements. padding, and margin of the grid containers and child elements.",
-        img: "https://example.com/bob-smith.jpg"
-      }
-    ];
 
 const Depart_Lead = () => {
   return (
     <>
-     <Members leader_name = "Arya" leader_info="she is more than awesome" leader_tech_stack="DSA" leader_img_location="Team_member_img" members={teamMembers} />
-
     <div className={styles.department}>
         <p className={styles.heading}>Our Department</p>
         <div className={styles.main}>
