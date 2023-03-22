28aed207f5f8c1f30e34b65d70abafbb6f423e4a
Pranshu321 <pranshujain0111@gmail.com>
2023-03-22 19:45:19 +0530

    Added the max-width and correcting the past and upcoming events

--- official-codechef/src/layouts/Layout.jsx
+++ official-codechef/src/layouts/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      {children}
+      <div style={{ maxWidth: "1500px", margin: "0 auto" }}>{children}</div>
       <Footer />
     </>
   );
