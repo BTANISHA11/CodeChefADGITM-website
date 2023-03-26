a2bd67af3c29a8619bcdfbb72e4378dc954c82cd
A1Kumari <aryakumari3953@gmail.com>
2023-03-26 19:36:33 +0530

    content uploaded

--- official-codechef/src/members/TeamMembers.jsx
+++ official-codechef/src/members/TeamMembers.jsx
@@ -1,37 +1,55 @@
 import React from "react";
+import { FaLinkedinIn } from 'react-icons/fa';
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
 import styles from "../styles/Members.module.css";
+import Link from "next/link";
+
 
 
 const Leader = ({leader_member_name, leader_tech_stack, leader_info}) => {
   return (
     <div className={styles.Leader_info}>
-      <div className={styles.leader_triangle}></div>
+    <div className={styles.leader_heading}>
       <h3>{leader_member_name}</h3>
       <p>{leader_tech_stack}</p>
+    </div>
       <hr className={styles.line} />
       <p>{leader_info}</p>
+      <hr className={styles.line} />
+      <div className={styles.socialLinks}>
+      <Link href="/"><FaLinkedinIn/></Link>
+      <Link href="/"><AiFillGithub/></Link>
+      <Link href="/"><AiFillInstagram/></Link>
+      </div>
     </div>
   );
 };
 
-const Member = ({team_member_name, tech_stack, team_info, img_location}) => {
+const Member = ({team_member_name, tech_stack, team_info, linkedin_link, github_link, insta_link}) => {
   return (
     <div className={styles.info}>
-      <div className={styles.triangle}></div>
-      <h3>{team_member_name}</h3>
-      <p>{tech_stack}</p>
+      <div className={styles.heading}>
+        <h3>{team_member_name}</h3>
+        <p>{tech_stack}</p>
+      </div>
       <hr className={styles.line} />
       <p className={styles.teamInfo}>{team_info}</p>
+      <hr className={styles.line} />
+      <div className={styles.socialLinks}>
+      <Link href="/"><FaLinkedinIn/></Link>
+      <Link href="/"><AiFillGithub/></Link>
+      <Link href="/"><AiFillInstagram/></Link>
+      </div>
     </div>
   ); 
 };
 
 const Members = ({leader_name, leader_info, leader_tech_stack, leader_img_location, members}) => {
    return (
-     <>
+     <div className={styles.Teampage}>
      <div className={styles.leader_grid}>
         <div className={styles.leader_information}>
-        <img src={leader_img_location}/>
+        <img src={leader_img_location} className={styles.leader_img}/>
           <Leader
             leader_member_name={leader_name}
             leader_info={leader_info}
@@ -43,17 +61,18 @@ const Members = ({leader_name, leader_info, leader_tech_stack, leader_img_locati
       <div className={styles.grid}>
         {members.map((member, index) => (
           <div key={index} className={styles.member_information}>
-            <img src={member.img} alt={member.name} />
+            <img src={member.img} alt={member.name} className={styles.member_img}/>
             <Member
               team_member_name={member.name}
               tech_stack={member.stack}
               team_info={member.info}
               
             />
+
           </div>
         ))}
       </div>
-     </>
+     </div>
    );
 }
 export default Members;
--- official-codechef/src/pages/team.jsx
+++ official-codechef/src/pages/team.jsx
@@ -1,40 +1,100 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import Depart_Lead from "@/components/Home/Depart_Lead.jsx";
+import Image from 'next/image';
 import Members from "../members/TeamMembers";
 
 const inter = Inter({ subsets: ["latin"] });
 const teamMembers = [
   {
-    name: "John Doe",
-    stack: "Frontend Developer",
-    info: "lorem ipsum",
-    img: "Team_member_img"
+    name: "Himanshu Gupta",
+    stack: "Event Lead",
+    info: `Events not only allow an engaging experience with the audience but also leaves a lasting impression on the organization... right?✨
+
+    Lets us introduce you to the person who brings every exciting event of society to you. He is a person with great enthusiasm and determination to make every event look just “perfect”.😃🤌🏻
+    
+    The Event Lead of Codechef ADGITM.... Himanshu Gupta 👏🥳
+    `,
+    img: "./images/Team/Team Leads/Himanshu.jpg"
   },
   {
-    name: "Arya",
-    stack: "Backend Developer",
-    info: "lorem ipsum",
-    img: "https://example.com/jane-doe.jpg"
+    name: "Aashi Aggarwal",
+    stack: "Vice president",
+    info: `With great power comes great responsibility…😌
+
+    Presenting to you the person who coordinate events with the president, oversee the internal matters with the leads…😮‍💨the individual who plans, ensures the team meets its targets and help us remain successful. 🥹
+    
+    Vice President– Aashi Aggarwal✨🌱`,
+    img: "./images/Team/Core Team/Aashi.jpg"
   },
   {
-    name: "Arya",
-    stack: "Backend Developer",
-    info: "lorem ipsum",
-    img: "https://example.com/jane-doe.jpg"
+    name: "Hitansh Gupta",
+    stack: "General Secretary",
+    info: `We all are set to overcome new milestones with a zest to spread out new technical colors…🎉
+
+    Let us introduce you to the person who ensures smooth running of board meetings, brings structure and discipline to our society...🙌🏻
+    
+    The General Secretary of Codechef ADGITM… Mr. Hitansh Gupta...✨🎉🥳
+    `,
+    img: "./images/Team/Core Team/Hitansh.jpg"
   },
   {
-    name: "Arya",
-    stack: "Designer",
-    info: "For the CSS, you can modify it as per your design requirements. padding, and margin of the grid containers and child elements.",
-    img: "https://example.com/bob-smith.jpg"
-  }
+    name: "Naman Patel",
+    stack: "Video editing Lead",
+    info: "lorem ipsum",
+    img: "./images/Team/Team Leads/Naman.jpg"
+  },
+  {
+    name: " Archita Jain",
+    stack: "Management Lead",
+    info: "lorem ipsum",
+    img: "./images/Team/Team Leads/Archita.jpg"
+  },
+  {
+    name: "Tanisha Bansal",
+    stack: "Marketing Lead ",
+    info: `With this, give a heartfelt welcome to our Internal affairs lead, "Ms. Tanisha Bansal" & "Mr. Veer Yadav". They are meticulous, tactical, and an excellent communicator who gets the job done.
+    💫They keeps themselves on top of budding trends and sees opportunities to capitalize on them, which is not an easy feat to achieve.💫`,
+    img: "./images/Team/Team Leads/Tanisha.jpg"
+  },
+  {
+    name: "Armaan Dutt",
+    stack: "Technical Lead",
+    info: "lorem ipsum",
+    img: "./images/Team/Team Leads/Armaan.jpg"
+  },
+  {
+    name: "Vanshika Goel",
+    stack: "Graphics Lead",
+    info: "lorem ipsum",
+    img: "./images/Team/Team Leads/Vanshika.png"
+  },
+  {
+    name: "Alokita Vij",
+    stack: "Content Lead",
+    info: "lorem ipsum",
+    img: "./images/Team/Team Leads/Alokita.jpg"
+  },
+  {
+    name: "Ansh Sachdeva",
+    stack: "Media Outreach head",
+    info: "lorem ipsum",
+    img: "./images/Team/Core Team/Ansh.jpg"
+  },
 ];
 
 const ManagementPage = () => {
   return(
   <>
-  <Members leader_name = "Arya" leader_info="she is more than awesome" leader_tech_stack="DSA" leader_img_location="Team_member_img" members={teamMembers} />
+  <Members 
+    leader_name = "Kartikey Gupta" 
+    leader_info="Leaders are those who keep a vision empower others...💫
+    One such leader you should definitely know is a person who started his journey being a member of CodeChef family and with his knack of dealing with every kind of situation, problem and situation, he empowers every one of us..
+    
+    Please give a round of applause to the president of *Codechef ADGITM* ... *Kartikey Gupta* 👏🏻🤩" 
+    leader_tech_stack="DSA" 
+    leader_img_location="./images/Team/Core Team/Kartikey.jpg" 
+    members={teamMembers} />
 </>
 )
 };
